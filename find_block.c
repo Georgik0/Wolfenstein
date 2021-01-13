db123711f740c3e22d0c9fbd8f4769019d1fe490
Stephane Kitsch <skitsch@ox-d5.msk.21-school.ru>
2021-01-13 21:02:55 +0300

    version 1.2

--- find_block.c
+++ find_block.c
@@ -35,6 +35,29 @@ double		get_dx(double phi)
 	return (dx);
 }
 
+double		get_dy(double phi)
+{
+	double dy;
+
+	if (phi == 0 || phi == M_PI)
+	{
+		dy = 0;
+	}
+	else if (phi == M_PI / 2)
+	{
+		dy = -64;
+	}
+	else if (phi == 3 * M_PI / 2)
+	{
+		dy = 64;
+	}
+	else
+	{
+		dy = 64 / tan(phi);
+	}
+	return (dy);
+}
+
 double		get_Ax(double phi, int player_x, int player_y, int Ay)
 {
 	double Ax;
@@ -50,14 +73,39 @@ double		get_Ax(double phi, int player_x, int player_y, int Ay)
 
 }
 
-double		horizontal_line(int player_x, int player_y)
+int		horizontal_line(int player_x, int player_y, double phi)
+{
+	int Ax;
+// 0 или pi
+	if (phi == 0)
+		Ax = (player_x / 64) * 64 + 64;
+	else
+		Ax = (player_x / 64) * 64 - 1;
+	return (Ax);
+}
+
+int		vertical_line(int player_x, int player_y, double phi)
+{
+	int Ay;
+// pi/2 или 3/2 * pi
+	if (phi == M_PI / 2)
+		Ay = (player_x / 64) * 64 - 1;
+	else
+		Ay = (player_x / 64) * 64 + 64;
+	return (Ay);
+}
+
+double		collision_width_vertical()
 {
 
 }
 
-double		vertical_line(int player_x, int player_y)
+double		collision_width_horisontal(char map[][10], double dx, double dy, double Ax, double Ay)
 {
+	while (map[][])
+	{
 
+	}
 }
 
 void	find_block(int player_x, int player_y, char map[][10], double phi)
@@ -69,15 +117,15 @@ void	find_block(int player_x, int player_y, char map[][10], double phi)
 
 	if (phi == M_PI / 2 || phi == 3 * M_PI / 2)
 	{
-
+		Ay = vertical_line(player_x, player_y, phi);
+		Ax = player_x;
 	}
 	else if (phi == 0 || phi == M_PI)
 	{
-
+		Ay = player_y;
+		Ax = horizontal_line(player_x, player_y, phi);
 	}
-
-
-	if (phi < M_PI)
+	else if (phi < M_PI)
 	{
 		Ay = (player_y / 64) * 64 - 1;
 		dy = -64;
@@ -87,6 +135,6 @@ void	find_block(int player_x, int player_y, char map[][10], double phi)
 		Ay = (player_y / 64) * 64 + 64;
 		dy = 64;
 	}
-	Ax = player_x + (player_y - Ay) / tan(phi);
+	dy = get_dy(phi);
 	dx = get_dx(phi);
 }
